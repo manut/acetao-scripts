bf0c338997daa6730eac98d42bdb3d79cfbfea6d
Manuel Traut <manut@mecka.net>
2013-05-15 09:03:29 +0200

    build tao with rtcorba support
    Signed-off-by: Manuel Traut <manut@mecka.net>

--- build-acetao.sh
+++ build-acetao.sh
@@ -18,6 +18,8 @@ echo include $ACE_ROOT/include/makeinclude/platform_linux.GNU > \
 	include/makeinclude/platform_macros.GNU
 echo 'qt4 = 1' >> include/makeinclude/platform_macros.GNU
 echo 'ace_qt4reactor = 1' >> include/makeinclude/platform_macros.GNU
+echo 'ace_for_tao = 1' >> include/makeinclude/platform_macros.GNU
+echo 'qos = 1' >> include/makeinclude/platform_macros.GNU
 echo "INSTALL_PREFIX = /usr" >> include/makeinclude/platform_macros.GNU
 echo "LDFLAGS += -fPIC" >> include/makeinclude/platform_macros.GNU
 echo "CFLAGS += -fPIC" >> include/makeinclude/platform_macros.GNU
@@ -29,22 +31,17 @@ ln -s $MPC_ROOT MPC
 unlink TAO
 ln -s $TAO_ROOT TAO
 
-#configure ACE
+# generate makefiles
+cp $ACE_ROOT/bin/MakeProjectCreator/config/* $MPC_ROOT/config/
 cp $ACE_ROOT/bin/MakeProjectCreator/modules/* $MPC_ROOT/modules/
-$ACE_ROOT/bin/mwc.pl -type gnuace ACE.mwc
+$ACE_ROOT/bin/mwc.pl -type gnuace $TAO_ROOT/TAO_ACE.mwc
 
-# configure TAO for use with ACE
+# build
+make -j1
 cd TAO
-$ACE_ROOT/bin/mwc.pl -type gnuace TAO_ACE.mwc
-cd -
+make -j1
 
-# build ACE
-make -j9
-
-# build/install TAO
-cd TAO
-make -j9
+# install
 sudo ACE_ROOT=$ACE_ROOT TAO_ROOT=$TAO_ROOT make install
 cd -
-# install ACE
 sudo ACE_ROOT=$ACE_ROOT TAO_ROOT=$TAO_ROOT make install
